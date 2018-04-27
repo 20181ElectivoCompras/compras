a2cea6d7460924fb30d8924de11f87787a7cd68d
USP <USP@C202-22>
2018-04-26 22:37:42 -0500

    HEAD

---
+++ head.php
@@ -0,0 +1,15 @@
+<head>
+    <meta charset="uf-8">
+    <title>
+        Compras - Grupo 1 
+    </title>
+    <link href="default.css" type="text/css" rel="stylesheet" media="screen" />
+</head>
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
