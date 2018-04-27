e54ac57308abf35707166b472a409af30721a265
USP <USP@C202-22>
2018-04-26 22:53:18 -0500

    INDEX

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <h1>COMPRAS</h1>
-<?php include('index.php'); ?>
-<head> 
-<title> Compras >> Grupo 1 </title>
 
 <?php include("head.php"); ?>
 <?php include("body.php"); ?>
 <?php include("footer.php"); ?>
 
-</head>
+
 
 
